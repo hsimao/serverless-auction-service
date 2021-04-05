b9f3b2ee3a99d95733f92f18ff5c5b757a4aa7ef
hsimao <e087754958@gmail.com>
2021-04-05 22:55:16 +0800

    feat: 新增取得拍賣結束的列表 logic

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,9 @@
+import { getEndedAuctions } from "../lib/getEndedAuctions";
+
 async function processAuctions(event, context) {
+  const auctionsToClose = await getEndedAuctions();
   console.log("processing auctions!");
+  console.log("close auctions: ", auctionsToClose);
 }
 
 export const handler = processAuctions;
